426812a5635a4e4fbf8d888906bfe0bbe59d7744
Tahlil <bsse0803@iit.du.ac.bd>
2022-06-28 23:01:56 +0600

    Setup frontend

---
+++ index.mjs
@@ -0,0 +1,8 @@
+import { loadStdlib} from '@reach-sh/stdlib';
+import * as backend from './build/index.main.mjs';
+
+const stdlib = loadStdlib();
+
+const startingBalance = stdlib.parseCurrency(100);
+const addAlice = await stdlib.newTestAccount(startingBalance);
+const addBob = await stdlib.newTestAccount(startingBalance);
