dd4df135587fbc1bc0976e91fae3984437dd13ab
Tahlil <bsse0803@iit.du.ac.bd>
2022-06-28 23:04:12 +0600

    Deploy contract

--- index.mjs
+++ index.mjs
@@ -6,3 +6,6 @@ const stdlib = loadStdlib();
 const startingBalance = stdlib.parseCurrency(100);
 const addAlice = await stdlib.newTestAccount(startingBalance);
 const addBob = await stdlib.newTestAccount(startingBalance);
+
+const ctcAlice = accAlice.contract(backend);
+const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
